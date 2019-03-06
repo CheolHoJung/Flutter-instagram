8ca62c65b159a32bbff9e8970123d760302d064e
jcheolho <fnel159@naver.com>
2019-03-06 23:00:24 +0900

    feat: First commit Add skeleton ui

---
+++ lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:instargram_clone/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
