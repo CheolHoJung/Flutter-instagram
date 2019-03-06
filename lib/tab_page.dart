ef7e2df556102b327f0e48ee731572526ac8a65f
jcheolho <fnel159@naver.com>
2019-03-07 00:25:37 +0900

    feat: Add SearchPage UI

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instargram_clone/account_page.dart';
 import 'package:instargram_clone/home_page.dart';
+import 'package:instargram_clone/search_page.dart';
 
 class TabPage extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class _TabPageState extends State<TabPage> {
 
   List _pages = [
     HomePage(),
-    Text('page2'),
+    SearchPage(),
     AccountPage(),
   ];
 
