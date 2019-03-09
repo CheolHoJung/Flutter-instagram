d4de5fe82c8b8c2049346cc6637b57bff31e66c0
jcheolho <fnel159@naver.com>
2019-03-10 02:01:03 +0900

    feat: Add LoginPage UI

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('login page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram Clon',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
