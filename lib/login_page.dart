5152707d4da7ed9b350c7d13addaae3e7ac9ce4e
jcheolho <fnel159@naver.com>
2019-03-10 02:59:51 +0900

    feat: Add google auth with Firebase

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/tab_page.dart';
 
 class LoginPage extends StatelessWidget {
+  final GoogleSignIn _googleSignin = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +24,27 @@ class LoginPage extends StatelessWidget {
             ),
             SignInButton(
               Buttons.Google,
-              onPressed: () {},
+              onPressed: () {
+                _handleSignIn().then((user) {
+                  Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => TabPage(user))
+                  );
+                });
+              },
             )
           ],
         ),
       ),
     );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignin.signIn();
+    GoogleSignInAuthentication googleAuth  = await googleUser.authentication;
+    FirebaseUser user = await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken)
+    );
+
+    return user;
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/login_page.dart';
 import 'package:instagram_clone/tab_page.dart';
@@ -5,6 +6,15 @@ import 'package:instagram_clone/tab_page.dart';
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return TabPage(snapshot.data);
+        } else {
+          return LoginPage();
+        }
+      },
+    );
   }
 }
