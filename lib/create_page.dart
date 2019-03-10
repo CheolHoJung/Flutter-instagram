8b8ab7216f35e74efe82eb2ed44ff85366138612
jcheolho <fnel159@naver.com>
2019-03-11 03:06:45 +0900

    feat: Add file upload to firebase storage and firestore

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CreatePage extends StatefulWidget {
+  final FirebaseUser user;
+
+  CreatePage(this.user);
+
   @override
   _CreatePageState createState() => _CreatePageState();
 }
@@ -38,7 +44,32 @@ class _CreatePageState extends State<CreatePage> {
         IconButton(
           icon: Icon(Icons.send),
           onPressed: () {
+            final firebaseStorageRef = FirebaseStorage.instance
+                .ref()
+                .child('post')
+                .child('${DateTime.now().millisecondsSinceEpoch}.png');
 
+            final task = firebaseStorageRef.putFile(
+              _image, StorageMetadata(contentType: 'image/png')
+            );
+
+            task.onComplete.then((value) {
+              var downloadURL = value.ref.getDownloadURL();
+
+              downloadURL.then((uri) {
+                var document = Firestore.instance.collection('post').document();
+                document.setData({
+                  'id': document.documentID,
+                  'photoUrl': uri.toString(),
+                  'contents': textEditingController.text,
+                  'email': widget.user.email,
+                  'displayName': widget.user.displayName,
+                  'userPhotoUrl': widget.user.photoUrl
+                }).then((onValue) {
+                  Navigator.pop(context);
+                });
+              });
+            });
           },
         )
       ],
